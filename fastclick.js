c696df1576cb69de5de82fed27f2503d493fe58c
kanyuan <281978870@qq.com>
2015-07-15 18:01:04 +0800

    修改成符合fis的模块

--- fastclick.js
+++ fastclick.js
@@ -813,16 +813,6 @@
 	};
 
 
-	if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
-
-		// AMD. Register as an anonymous module.
-		define(function() {
-			return FastClick;
-		});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = FastClick.attach;
-		module.exports.FastClick = FastClick;
-	} else {
-		window.FastClick = FastClick;
-	}
+	module.exports = FastClick.attach;
+	module.exports.FastClick = FastClick;
 }());
